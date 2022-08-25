ec1b31f1f7b929975411b696cf1c825d6358278d
JiaoKan <realJiaoKan@icloud.com>
2022-08-26 04:49:31 +0800

    database

---
+++ plugins/arcaea/database/database.py
@@ -0,0 +1,52 @@
+from nonebot.log import logger
+from pathlib import Path
+from os import path
+import peewee as pw
+
+db = pw.SqliteDatabase(Path(__file__).parent.absolute() / "user_data.db")
+db_root = Path(__file__).parent.absolute()
+
+
+class UserInfo(pw.Model):
+    user_qq = pw.IntegerField()
+    arcaea_id = pw.CharField()
+
+    class Meta:
+        database = db
+        primary_key = pw.CompositeKey("user_qq", "arcaea_id")
+
+
+class ArcInfo(pw.Model):
+    arcaea_name = pw.CharField()
+    arcaea_id = pw.CharField()
+    ptt = pw.IntegerField()
+
+    class Meta:
+        database = db
+        primary_key = pw.CompositeKey("arcaea_name", "arcaea_id", "ptt")
+
+
+class UserConfig(pw.Model):
+    user_qq = pw.IntegerField()
+    single_song_theme = pw.CharField()
+    best30_theme = pw.CharField()
+
+    class Meta:
+        database = db
+        primary_key = pw.CompositeKey("user_qq", "single_song_theme", "best30_theme")
+
+
+if not path.exists(db_root / "user_data.db"):
+    logger.info(f"创建数据库于 {db_root / 'user_data.db'}")
+    db.connect()
+    db.create_tables([UserInfo, ArcInfo, UserConfig])
+    db.close()
+else:
+    db.connect()
+    if "userinfo" not in db.get_tables():
+        db.create_tables([UserInfo])
+    if "userconfig" not in db.get_tables():
+        db.create_tables([UserConfig])
+    if "arcinfo" not in db.get_tables():
+        db.create_tables([ArcInfo])
+    db.close()
